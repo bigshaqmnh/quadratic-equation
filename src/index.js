3c0682873a0e7627bef62e3b7ea503b8b8a17ea0
Michael <kotikov.misha@gmail.com>
2018-09-13 07:40:36 +0300

    math revised!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let substringArray = equation.split(' ');
+
+  let a = +substringArray[0], b, c;
+
+  if (substringArray[3] === '-') {
+    b = - (+substringArray[4]);
+  } else {
+    b = +substringArray[4];
+  }
+
+  if (substringArray[7] === '-') {
+    c = - (+substringArray[8]);
+  } else {
+    c = +substringArray[8];
+  }
+
+  const discriminant = (a, b, c) => b * b - 4 * a * c;
+
+  let x1 = ((-b) + Math.sqrt(discriminant(a, b, c))) / (2 * a);
+  let x2 = ((-b) - Math.sqrt(discriminant(a, b, c))) / (2 * a); 
+
+  let resArr = [Math.round(x1), Math.round(x2)];
+
+  return resArr.sort((x1, x2) => x1 - x2);
 }
